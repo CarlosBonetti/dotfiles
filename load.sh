5391e92b01794bbb387c5eabeba5e947c3f78133
Carlos Bonetti <carlosb_bc@hotmail.com>
2014-07-27 21:07:58 -0400

    Loading procedures now stored on load.sh. Calling it on every rc file

---
+++ load.sh
@@ -0,0 +1,12 @@
+# Generic load file
+# Might be executed by any `*rc` file (`.bashrc`, `.zshrc`, ...)
+
+# Includes user's private bin in PATH if it exists
+if [ -d "$HOME/bin" ] ; then
+  PATH="$HOME/bin:$PATH"
+fi
+
+# Executes every `*.load` file inside the dotfiles structure
+for file in $(find "$DOTFILES_HOME" -name "*.load"); do
+  source "$file"
+done
