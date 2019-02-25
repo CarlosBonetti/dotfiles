184c0274e4d6d30602a4e6fcdd5ed676eb366c7b
Carlos Bonetti <carlosb_bc@hotmail.com>
2019-02-25 08:58:44 -0300

    Move load scripts to load.sh file

--- load.sh
+++ load.sh
@@ -11,5 +11,17 @@ for file in $(find "$DOTFILES_HOME" -name "*.load"); do
   source "$file"
 done
 
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # Yarn
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+# rvm
+export PATH="$PATH:$HOME/.rvm/bin"
+
+# sdkman
+export SDKMAN_DIR="/home/bonetti/.sdkman"
+[[ -s "/home/bonetti/.sdkman/bin/sdkman-init.sh" ]] && source "/home/bonetti/.sdkman/bin/sdkman-init.sh"
